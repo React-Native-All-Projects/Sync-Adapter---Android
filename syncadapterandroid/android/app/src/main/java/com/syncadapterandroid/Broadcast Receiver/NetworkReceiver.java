e76f603edabc0418e02fc0354b9f7a23e1059027
Osama Al-Daja <o.aldajeh.ext@tahaluf.ae>
2022-03-30 22:59:12 +0200

    Final

--- syncadapterandroid/android/app/src/main/java/com/syncadapterandroid/Broadcast Receiver/NetworkReceiver.java
+++ syncadapterandroid/android/app/src/main/java/com/syncadapterandroid/Broadcast Receiver/NetworkReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import com.facebook.react.bridge.ReactApplicationContext;
 
 public class NetworkReceiver extends BroadcastReceiver {
     @Override
